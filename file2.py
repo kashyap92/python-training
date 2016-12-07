0219a8075b52e1247193da0705c4f55896f4046f
kashyap92 <kashyap022@yahoo.in>
2016-12-07 14:32:04 +0530

    Create file2.py

---
+++ file2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+#file=raw_input("enter the file name:")
+#filename=sys.argv[0]
+file1=open("MyMath.py")
+#lines=file1.readline()
+a=1
+while  in file1:
+        a+=1
+        print a,i
+        #lines=file1.readline()
+        #a+=1
+file1.close()
