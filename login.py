8df96647aa6b53e47fdf1e615e73a35bca8254ef
kashyap92 <kashyap022@yahoo.in>
2016-12-07 14:28:49 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+username=raw_input("Enter username:")
+password=input("password:")
+if ( username == 'mahesh' and password == 123 ) :
+        print("login successful")
+else :
+        print("Login failed")
