5486aaa7036d1624e8fff94964b4c8f7cf4591b7
kashyap92 <kashyap022@yahoo.in>
2016-12-07 14:25:16 +0530

    Create break.py

---
+++ break.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+ctr=0
+while ctr < 14 :
+        ctr+=1
+        if ctr == 15 :
+                break
+        print ctr
+else:
+        print "Outside while"
+        print ctr
