9a535bf206a99e06b588131331648f5735556274
kashyap92 <kashyap022@yahoo.in>
2016-12-07 14:31:25 +0530

    Create MyMath.py

---
+++ MyMath.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+def add(a,b):
+        return a+b
+def mul(a,b):
+        return a*b
+def sub(a,b):
+        return a-b
+def div(a,b):
+        return a/b
+if __name__ == "__main__":
+        print add(10,20)
+        print mul(10,20)
+        print sub(4,2)
+        print div(10,2)
+print __name__
